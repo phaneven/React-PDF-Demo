51233a7152822fb385354c2b03562579b8324d82
phaneven <phaneven@gmail.com>
2018-12-10 15:40:58 +0800

    fix SheetViewer/Viewer.js

--- src/SheetViewer/Viewer.js
+++ src/SheetViewer/Viewer.js
@@ -1,8 +1,4 @@
 /* eslint-disable react/no-array-index-key */
-/* eslint-disable jsx-a11y/label-has-for */
-/* eslint-disable jsx-a11y/label-has-associated-control */
-/* eslint-disable no-console */
-/* eslint-disable react/no-unused-state */
 import React from 'react';
 import PropTypes from 'prop-types';
 
@@ -29,38 +25,27 @@ class Sheet extends React.Component {
     this.handleFile();
   }
 
-  // eslint-disable-next-line class-methods-use-this
   handleFile() {
     const { url } = this.props;
-    // url = '/public/file.xlsx';
     const req = new XMLHttpRequest();
     req.open('GET', url, true);
     req.responseType = 'arraybuffer';
     req.onload = () => {
-      console.log(req.response);
       const data = new Uint8Array(req.response);
       const wb = XLSX.read(data, { type: 'array', cellStyles: true });
       const wsname = wb.SheetNames[0];
       const ws = wb.Sheets[wsname];
-      // const originGrid = XLSX.utils.sheet_to_json(ws, { header: 1 });
-      // const HTML = XLSX.utils.sheet_to_html(ws);
-      // document.getElementById('root').innerHTML = HTML;
       const grid = XLSX.utils.sheet_to_json(ws, { header: 1 });
-      console.log(grid);
-      console.log(makeCols(ws['!ref']));
       this.setState({ data: grid, cols: makeCols(ws['!ref']) });
     };
     req.send();
   }
 
   render() {
-    // return <div>test</div>;
-    // const { grid } = this.state;
-    // console.log(grid);
     const { cols, data } = this.state;
     return (
       <div>
-        <div className="table-responsive">
+        <div className="table-responsive-sm">
           <table className="table table-striped">
             <thead>
               <tr>{cols.map(c => <th key={c.key}>{c.name}</th>)}</tr>
