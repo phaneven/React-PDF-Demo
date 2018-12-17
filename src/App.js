29a558dc48f7f410e730e7df2b3381f0c6ddf2cc
phaneven <phaneven@gmail.com>
2018-12-17 10:06:03 +0800

    finish basic sheet previewer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import PDFViewer from './PDFViewer';
 import ExcelViewer from './SheetViewer/Viewer';
+import Aux from './hoc/Aux';
 
 const App = () => {
   const check = 'excel';
   return (
-    <div>
+    <Aux>
       {check === 'pdf' && <PDFViewer url="/public/BayesTheorem.pdf" />}
       {check === 'excel' && <ExcelViewer url="/public/file.xlsx" />}
-    </div>
+    </Aux>
   );
 };
 
--- src/SheetViewer/Viewer.js
+++ src/SheetViewer/Viewer.js
@@ -1,6 +1,9 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
 import PropTypes from 'prop-types';
+// import classNames from 'classnames';
+import './style/index.less';
 
 const XLSX = require('xlsx');
 
@@ -17,6 +20,9 @@ class Sheet extends React.Component {
     this.state = {
       data: [],
       cols: [],
+      wb: null,
+      sheetNames: [],
+      selected: '',
     };
     this.handleFile = this.handleFile.bind(this);
   }
@@ -35,29 +41,81 @@ class Sheet extends React.Component {
       const wb = XLSX.read(data, { type: 'array', cellStyles: true });
       const wsname = wb.SheetNames[0];
       const ws = wb.Sheets[wsname];
-      const grid = XLSX.utils.sheet_to_json(ws, { header: 1 });
-      this.setState({ data: grid, cols: makeCols(ws['!ref']) });
+      const grid = XLSX.utils.sheet_to_json(ws, { header: 'A' });
+      this.setState({
+        data: grid,
+        cols: makeCols(ws['!ref']),
+        wb,
+        sheetNames: wb.SheetNames,
+        selected: wb.SheetNames[0],
+      });
     };
     req.send();
   }
 
+  selectSheet(wsname) {
+    const { wb } = this.state;
+    const ws = wb.Sheets[wsname];
+    const grid = XLSX.utils.sheet_to_json(ws, { header: 'A' });
+    this.setState({
+      data: grid,
+      cols: makeCols(ws['!ref']),
+      selected: wsname,
+    });
+  }
+
   render() {
-    const { cols, data } = this.state;
+    const {
+      cols,
+      data,
+      sheetNames,
+      selected,
+    } = this.state;
+    const { url } = this.props;
     return (
-      <div>
-        <div className="table-responsive-sm">
-          <table className="table table-striped">
+      <div className="xlsx-container">
+        <div className="xlsx-file">
+          {url.replace('/public/', '')}
+        </div>
+        <div className="table-container">
+          <table className="table">
             <thead>
-              <tr>{cols.map(c => <th key={c.key}>{c.name}</th>)}</tr>
+              <tr>
+                <th>#</th>
+                {cols.map(c => <th key={c.key}>{c.name}</th>)}
+              </tr>
             </thead>
             <tbody>
-              {data.map((r, i) => (
-                <tr key={i}>
-                  {cols.map(c => <td key={c.key}>{ r[c.key] }</td>)}
-                </tr>
-              ))}
+              {
+                data.map(item => (
+                  <tr key={item.__rowNum__}>
+                    <td>{item.__rowNum__ + 1}</td>
+                    {
+                      cols.map(c => <td key={c.key}>{item[c.name]}</td>)
+                    }
+                  </tr>
+                ))
+              }
             </tbody>
           </table>
+        </div>
+        <div className="sheet-selector">
+          {
+            sheetNames.map(name => (
+              <div
+                role="button"
+                tabIndex={0}
+                className={name === selected ? 'sheet-name sheet-name-selected' : 'sheet-name'}
+                key={name}
+                onKeyPress={() => {}}
+                onClick={() => {
+                  this.selectSheet(name);
+                }}
+              >
+                {name}
+              </div>
+            ))
+          }
         </div>
       </div>
     );
